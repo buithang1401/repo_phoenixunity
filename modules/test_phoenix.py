e8c4d9bde34c62c45b50a3628e1973050da1a2e5
buithang1401 <thang.dev.1401@gmail.com>
2023-10-22 16:31:27 +0700

    push test

---
+++ modules/test_phoenix.py
@@ -0,0 +1,4 @@
+username = 'user1'
+password = '123123123'
+
+note = 'day la data de test he thong drp'
---
+++ phoenixunity/config.py
@@ -0,0 +1,5 @@
+import readline
+import datetime
+
+password = 'daylapasstest'
+username = 'test123'
